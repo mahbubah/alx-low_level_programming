e905ba7fb8eeda49efbccd1f91c602d9a936d345
mahbubah <mahbubah.abdu>
2023-02-17 05:05:05 -0800

    Print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

66902a6158492d6b646d47496c7af64cb0682ac0
mahbubah <mahbubah.abdu>
2023-02-23 03:41:03 -0800

    More functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+* main - check if a character is uppercase letter
+* Return : Always 0
+*/
+
+int main(void)
+{
+	char c;
+
+	while (isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Readme file on more functions nested loops

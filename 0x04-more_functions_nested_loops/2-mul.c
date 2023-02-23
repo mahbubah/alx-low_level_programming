78a36cab91bcd352eada7cb76513d61b1f85068c
mahbubah <mahbubah.abdu>
2023-02-23 04:13:11 -0800

    More functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies numbers inputted
+ * @a : input number
+ * @b : input number
+ * Return : the result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
